2f6e05099c2a2059088754b77b5632a0b972a9dd
Danish Malik: git config --global user.name Danish <danishahmed888@gmail.com>
2023-08-31 13:06:38 +0530

    Add the sliding feature

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -23,12 +23,12 @@ const data = [
 const Slider = () => {
   const[slider, setSlider] = useState(0) 
 
-//   useEffect(()=> {
-//     const interval = setInterval(()=>{ 
-//       setSlider((prev)=> (prev === data.length-1 ? 0 : prev+1 ))
-//     },2000)
-//     return ()=> clearInterval(interval); 
-// },[])
+  useEffect(()=> {
+    const interval = setInterval(()=>{ 
+      setSlider((prev)=> (prev === data.length-1 ? 0 : prev+1 ))
+    },2000)
+    return ()=> clearInterval(interval); 
+},[])
   
 
   return (
